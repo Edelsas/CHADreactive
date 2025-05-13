32fb61f1dcd322537f2ec2a7b46c1370ae4eac41
Sticarto <123905512+sticarto@users.noreply.github.com>
2025-05-12 17:14:05 -0700

    Update register page UI

--- app/register.jsx
+++ app/register.jsx
@@ -1,5 +1,6 @@
 import { useRouter } from "expo-router";
-import { Text, View, StyleSheet, SafeAreaView, TextInput, Button, TouchableHighlight} from "react-native";
+import { Text, View, StyleSheet, SafeAreaView, TextInput, KeyboardAvoidingView, Platform, TouchableHighlight, Keyboard} from "react-native";
+import { TouchableWithoutFeedback } from "react-native-web";
 
 export default function Register() {
     const router = useRouter();
@@ -9,49 +10,61 @@ export default function Register() {
     }
 
     return (
-        <SafeAreaView>
-            <View style={styles.card}>
-                <Text style={styles.title}>Register</Text>
-                
-                <TextInput style={styles.input} placeholder="First Name" placeholderTextColor={"grey"} />
-                <TextInput style={styles.input} placeholder="Last Name" placeholderTextColor={"grey"} />
-                <TextInput style={styles.input} placeholder="Email" placeholderTextColor={"grey"}/>
-                <TextInput style={styles.input} placeholder="Password" placeholderTextColor={"grey"}/>
-                <TextInput style={styles.input} placeholder="Confirm Password" placeholderTextColor={"grey"}/>
+        <SafeAreaView style={styles.screen}>
+            <KeyboardAvoidingView 
+                behavior={Platform.OS === 'ios' ? 'padding': 'height'}
+                style={styles.card}
+            >
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <View>
+                        <Text style={styles.title}>Register</Text>
+                        
+                        <TextInput style={styles.input} placeholder="First Name" placeholderTextColor={"grey"} />
+                        <TextInput style={styles.input} placeholder="Last Name" placeholderTextColor={"grey"} />
+                        <TextInput style={styles.input} placeholder="Email" placeholderTextColor={"grey"}/>
+                        <TextInput style={styles.input} placeholder="Password" placeholderTextColor={"grey"}/>
+                        <TextInput style={styles.input} placeholder="Confirm Password" placeholderTextColor={"grey"}/>
 
-                <TouchableHighlight onPress={onPress} style={styles.registerButton} underlayColor={'rgb(47, 131, 188)'}>
-                    <Text style={styles.buttonText}>Register</Text>
-                </TouchableHighlight>
+                        <TouchableHighlight onPress={onPress} style={styles.registerButton} underlayColor={'rgb(47, 131, 188)'}>
+                            <Text style={styles.buttonText}>Register</Text>
+                        </TouchableHighlight>
 
-                <TouchableHighlight onPress={() => router.back()} style={styles.backButton} underlayColor={'rgba(188, 47, 47, 0.64)'}>
-                    <Text style={styles.buttonText}>Back</Text>
-                </TouchableHighlight>
-                
-            </View>
+                        <TouchableHighlight onPress={() => router.back()} style={styles.backButton} underlayColor={'rgba(188, 47, 47, 0.64)'}>
+                            <Text style={styles.buttonText}>Back</Text>
+                        </TouchableHighlight>
+                    </View>
+                </TouchableWithoutFeedback>
+            </KeyboardAvoidingView>
 
         </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-    login: {
+    screen: {
+        backgroundColor: '#1a1a1a',
         flex: 1,
-        justifyContent: "center",
+        justifyContent:'center',
     },
     title: {
         fontSize: 34,
         fontWeight: "bold",
-        paddingVertical: 20 
+        paddingVertical: 20,
+        color: 'white'
     },
     card: {
-        margin: 50,
+        padding: 30,
+        backgroundColor: '#333',
+        borderRadius: 16
     },
     input: {
         height: 40,
         marginVertical: 10,
         padding: 10,
         borderWidth: 1,
-        borderRadius: 4
+        borderRadius: 4,
+        borderColor: '#555',
+        color: 'white'
     },
     registerButton: {
         backgroundColor: 'rgb(70, 181, 255)',
